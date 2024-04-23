f5d21957b7c09bdff99739c68fc353d96d63fe90
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-04-23 20:34:40 +0530

    Init

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-green-600">
+        Hello world!
+      </h1>
+
+     
+    </div>
+
+
+
+  );
+}
