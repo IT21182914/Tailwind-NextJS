c62ee1490ceadcb50fd4d69de622ab195c335a35
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-04-24 01:08:14 +0530

    sm,lg

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="border-4">
+      <h1 className="text-sm lg:text-lg">
         Hello world!
       </h1>
 
-      <h1 className="text-vuejs-100">
+      <h1 className="text-vuejs-light">
         Hello world!
       </h1>
 
