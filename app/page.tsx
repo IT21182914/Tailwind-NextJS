e54fb7cb278de7c79864e10bad17bb46eb1b327c
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-04-26 07:47:40 +0530

    flex and grow

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-sm lg:text-lg">
-        Hello world!
-      </h1>
-
-      <h1 className="text-vuejs-light">
-        Hello world!
-      </h1>
+    <div className="Parent">
+  
+        <div className="">
+       Header
+        </div>
+        <div className="flex">
+          <div className="grow">Main Content</div>
+          <div className="flex flex-col">
+            <div>Side Bar</div>
+            <div>Menu</div>
+          </div>
+        </div>
 
     </div>
-
 
 
   );
